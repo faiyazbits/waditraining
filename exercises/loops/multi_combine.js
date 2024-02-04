7063f035790fe89d4b9e9fe435a84c43e3e6db85
naziya-usman <Naziyausmantwaiba@gmail.com>
2024-02-04 12:31:30 +0530

    completed sum.js

--- exercises/loops/multi_combine.js
+++ exercises/loops/multi_combine.js
@@ -3,11 +3,20 @@
  * generate a string
  * Expected Result: (['A', 'B', 'C'],['1', '2', '3', '4', '5', '6', '7', '8'],['a', 'e', 'i', 'o', 'u']) => 'A1aB2eC3i4o5u678'
  * Task Complexity: 1 of 5
+ * A1aB2eC3i4o5u678
+ *
  */
 const characters = ["A", "B", "C"];
 const numbers = ["1", "2", "3", "4", "5", "6", "7", "8"];
 const vowels = ["a", "e", "i", "o", "u"];
 
-const multi_combine = (characters, numbers, vowels) => {};
+const multi_combine = (characters, numbers, vowels) => {
+  const mergedArray = [];
 
-console.log(multi_combine(characters, numbers, vowels));
+  for (let i = 0; i < characters.length; i++) {
+    mergedArray.push(characters[i] + numbers[i] + vowels[i]);
+  }
+  console.log(mergedArray.join(""));
+};
+
+multi_combine(characters, numbers, vowels);
