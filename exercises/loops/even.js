48f5b5a93b5d56edbddd6d3e66ba57e4c09b2990
fathimamn <fathimamnwaditaiba@gmail.com>
2024-02-04 11:24:01 +0530

    completed even.js

--- exercises/loops/even.js
+++ exercises/loops/even.js
@@ -1,9 +1,14 @@
 /**
-  * Task description: Write a method that to print a even number upto a size only without conditional statement using loop.
-  * Expected Result: (15) => 0,2,4,6,8,10,12,14
-  * Task Complexity: 1 of 5
-*/
+ * Task description: Write a method that to print a even number upto a size only without conditional statement using loop.
+ * Expected Result: (15) => 0,2,4,6,8,10,12,14
+ * Task Complexity: 1 of 5
+ */
 const printEvenNumbers = (size) => {
-}
-const size = 15
-console.log(printEvenNumbers(size)) // 0,2,4,6,8,10,12,14
+  for (let i = 0; i <= size; i++) {
+    if (i % 2 == 0) {
+      console.log(i);
+    }
+  }
+};
+const size = 15;
+printEvenNumbers(size); // 0,2,4,6,8,10,12,14
