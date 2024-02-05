b5c9b902a748ed76936b5f86b4f26d0d6860cd34
fathimamn <fathimamnwaditaiba@gmail.com>
2024-02-05 11:15:03 +0530

    completed check-blank.js

--- exercises/string/check-blank.js
+++ exercises/string/check-blank.js
@@ -1,9 +1,10 @@
 /**
-  * Task description: Write a method to check whether a string is blank or not
-  * Expected Result: ('') => true
-  * Task Complexity: 1 of 5
-*/
+ * Task description: Write a method to check whether a string is blank or not
+ * Expected Result: ('') => true
+ * Task Complexity: 1 of 5
+ */
 const checkIsBlank = (str) => {
-}
-const str = '';
-console.log(checkIsBlank(str)) // true
+  console.log(str);
+};
+const str = "true";
+checkIsBlank(str); // true
