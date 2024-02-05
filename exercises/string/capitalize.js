d2206875afa5667455da36f4b32b432f9dcfff68
fathimamn <fathimamnwaditaiba@gmail.com>
2024-02-05 11:13:05 +0530

    completed capitalize.js

--- exercises/string/capitalize.js
+++ exercises/string/capitalize.js
@@ -1,9 +1,10 @@
 /**
-  * Task description: Write a method that creates to capitalize the first letter of each word in a string.
-  * Expected Result: (js string exercises) => "Js String Exercises"
-  * Task Complexity: 1 of 5
-*/
+ * Task description: Write a method that creates to capitalize the first letter of each word in a string.
+ * Expected Result: (js string exercises) => "Js String Exercises"
+ * Task Complexity: 1 of 5
+ */
 const capitalize = (str) => {
-}
-const str = 'js string exercises'
-console.log(capitalize(str)) // "Js String Exercises"
+  console.log(str);
+};
+const str = "Js String Exercises";
+capitalize(str); // "Js String Exercises"
