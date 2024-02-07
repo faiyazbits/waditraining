147dd965f22e57526a96918c81a2f3fe29e5d1bd
fathimamn <fathimamnwaditaiba@gmail.com>
2024-02-07 11:57:29 +0530

    working capitalize.js

--- exercises/string/capitalize.js
+++ exercises/string/capitalize.js
@@ -4,15 +4,22 @@
  * Task Complexity: 1 of 5
  */
 const capitalize = (str) => {
-  let s = str.split(" ");
-  let n = [];
-  for (let i = 0; i < s.length; i++) {
-    let f = s[i];
-    let word = f[0].toUpperCase();
-
-    console.log(word);
+  let newarray = [];
+  let array = str.split(" ");
+  for (let i = 0; i < array.length; i++) {
+    let word = array[i];
+    let capitelLetter = word[0];
+    let uppercaseWord = capitelLetter.toUpperCase();
+    let changeword = replaceChar(word, 0, uppercaseWord);
+    return newarray.push(changeword);
   }
+  return changeword.join(" ");
 };
+function replaceChar(str, index, char) {
+  const array = str.split("");
+  array[index] = char;
+  return array.join("");
+}
 const str = "js string exercises";
-capitalize(str); // "Js String Exercises"
-console.log(word);
+let output = capitalize(str); // "Js String Exercises"
+console.log(output);
