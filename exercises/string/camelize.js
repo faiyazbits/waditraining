28cb01fa5d03e87e53716a9ecb6ed5e9cd6eec42
aznamolkn123 <aznawaditwaiba@gmail.com>
2024-02-08 11:27:03 +0530

    complete check-string.js

--- exercises/string/camelize.js
+++ exercises/string/camelize.js
@@ -3,20 +3,7 @@
  * Expected Result: (JavaScript exercises) => "JavaScriptExercises"
  * Task Complexity: 1 of 5
  */
-const camelize = (str) => {
-  let words = str.split(" ");
-  let word = words[1];
-  let firstCharecter = word[0];
-  let capitalLetter = firstCharecter.toUpperCase();
-  let changedWord = replaceChar(word, 0, capitalLetter);
-  words[1] = changedWord;
-};
-function replaceChar(str, index, char) {
-  const array = str.split("");
+const camelize = (str) => {};
 
-  array[index] = char;
-
-  return array.join("");
-}
 const str = "JavaScript exercises";
 console.log(camelize(str)); // "JavaScriptExercises"
