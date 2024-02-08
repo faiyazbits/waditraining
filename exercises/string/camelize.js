9d665d63b015ddc4cd6b7c0b745b8e1c6acaa26a
aznamolkn123 <aznawaditwaiba@gmail.com>
2024-02-08 10:46:47 +0530

    complete check-blank.js

--- exercises/string/camelize.js
+++ exercises/string/camelize.js
@@ -1,9 +1,22 @@
 /**
-  * Task description: Write a method to convert a string into camel case.
-  * Expected Result: (JavaScript exercises) => "JavaScriptExercises"
-  * Task Complexity: 1 of 5
-*/
+ * Task description: Write a method to convert a string into camel case.
+ * Expected Result: (JavaScript exercises) => "JavaScriptExercises"
+ * Task Complexity: 1 of 5
+ */
 const camelize = (str) => {
+  let words = str.split(" ");
+  let word = words[1];
+  let firstCharecter = word[0];
+  let capitalLetter = firstCharecter.toUpperCase();
+  let changedWord = replaceChar(word, 0, capitalLetter);
+  words[1] = changedWord;
+};
+function replaceChar(str, index, char) {
+  const array = str.split("");
+
+  array[index] = char;
+
+  return array.join("");
 }
-const str = 'JavaScript exercises'
-console.log(camelize(str)) // "JavaScriptExercises"
+const str = "JavaScript exercises";
+console.log(camelize(str)); // "JavaScriptExercises"
--- exercises/string/check-blank.js
+++ exercises/string/check-blank.js
@@ -1,9 +1,14 @@
 /**
-  * Task description: Write a method to check whether a string is blank or not
-  * Expected Result: ('') => true
-  * Task Complexity: 1 of 5
-*/
+ * Task description: Write a method to check whether a string is blank or not
+ * Expected Result: ('') => true
+ * Task Complexity: 1 of 5
+ */
 const checkIsBlank = (str) => {
-}
-const str = '';
-console.log(checkIsBlank(str)) // true
+  if (str === "") {
+    return true;
+  } else {
+    return false;
+  }
+};
+const str = "";
+console.log(checkIsBlank(str)); // true
