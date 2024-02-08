3ecf04fa5d25f7653e9bf4d296682070e95b2b01
sulthananazeer <sulthanawaditwaiba@gmail.com>
2024-02-08 11:54:07 +0530

    working on revers.js

--- exercises/arrays/reverse.js
+++ exercises/arrays/reverse.js
@@ -1,12 +1,14 @@
-/** 
-  * Task description: Write a method that reverts input array 
-  * Expected Result: [1, 2, 3] => [3, 2, 1] 
-  * Task Complexity: 1 of 5 
-  * @param {Array} array - Array of any elements 
-  * @returns {Array} 
-*/
+/**
+ * Task description: Write a method that reverts input array
+ * Expected Result: [1, 2, 3] => [3, 2, 1]
+ * Task Complexity: 1 of 5
+ * @param {Array} array - Array of any elements
+ * @returns {Array}
+ */
 const reverse = (array) => {
-    throw new Error('Put your solution here');
-}
+  for (let i = reverse.length - 1; i >= 0; i--) {
+    reverse.push([i]);
+  }
+};
 const data = [1, 2, 3];
 console.log(reverse(data)); // [3, 2, 1]
