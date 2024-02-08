2131ab34fb22242bf18c6c5c3590f9e6395d63cd
sulthananazeer <sulthanawaditwaiba@gmail.com>
2024-02-08 10:44:06 +0530

    completed the task using fill method

--- exercises/arrays/fill.js
+++ exercises/arrays/fill.js
@@ -1,10 +1,17 @@
-/** 
-  * Task description: Write a method that creates a new array with given values 
-  * Expected Result: (3, 'a') => ['a', 'a', 'a'] 
-  * Task Complexity: 1 of 5 
-*/
+/**
+ * Task description: Write a method that creates a new array with given values
+ * Expected Result: (3, 'a') => ['a', 'a', 'a']
+ * Task Complexity: 1 of 5
+ */
 const fill = (arraySize, value) => {
-}
+  let array = [];
+  for (let i = 0; i < arraySize; i++) {
+    array.push(value);
+  }
+  return array;
+};
+
+fill();
 const data = 3;
-const valueToFill = 'a';
-console.log(fill(data, valueToFill)) // ['a', 'a', 'a']
+const valueToFill = "a";
+console.log(fill(data, valueToFill)); // ['a', 'a', 'a']
