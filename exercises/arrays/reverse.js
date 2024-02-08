df17efe3309c6e4c81dc7752873ccde2d40c4719
sajinafathima <waditwaibasajina@gmail.com>
2024-02-08 11:52:44 +0530

    complete reverse.js

--- exercises/arrays/reverse.js
+++ exercises/arrays/reverse.js
@@ -5,24 +5,13 @@
  * @param {Array} array - Array of any elements
  * @returns {Array}
  */
-// const reverse = (array) => {
-//   for (let i = 3; i < array.length; i--) {
-//     revered_array.push(array);
-//   }
-// };
-// const data = [1, 2, 3];
-// console.log(reverse(data)); // [3, 2, 1]
+let revered_array = [];
 
-let original_array = [1, 2, 3, 4];
-
-let reversed_array = [];
-
-console.log("Original Array: ");
-console.log(original_array);
-
-for (let i = original_array.length - 1; i >= 0; i--) {
-  reversed_array.push(original_array[i]);
-}
-
-console.log("Reversed Array: ");
-console.log(reversed_array);
+const reverse = (array) => {
+  for (let i = array.length - 1; i >= 0; i--) {
+    revered_array.push(array[i]);
+  }
+  console.log(revered_array);
+};
+const data = [1, 2, 3];
+reverse(data); // [3, 2, 1]
