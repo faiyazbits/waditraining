0e0d4b6806aebc45b005631d0227ffe4bacd1f8c
aznamolkn123 <aznawaditwaiba@gmail.com>
2024-02-08 11:32:12 +0530

    complete fill.js

--- exercises/arrays/fill.js
+++ exercises/arrays/fill.js
@@ -3,7 +3,13 @@
  * Expected Result: (3, 'a') => ['a', 'a', 'a']
  * Task Complexity: 1 of 5
  */
-const fill = (arraySize, value) => {};
+const fill = (arraySize, value) => {
+  let newArray = [];
+  for (let i = 0; i < arraySize; i++) {
+    newArray.push(value);
+  }
+  return newArray;
+};
 const data = 3;
 const valueToFill = "a";
 console.log(fill(data, valueToFill)); // ['a', 'a', 'a']
