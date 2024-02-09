5dcffbf39e5588ceff5e5312957563e93b5d142e
naziya-usman <Naziyausmantwaiba@gmail.com>
2024-02-09 11:47:13 +0530

    not completed

--- exercises/objects/from_pairs.js
+++ exercises/objects/from_pairs.js
@@ -6,10 +6,22 @@
  * @returns {Array}
  */
 const fromPairs = (array) => {
-  console.log(array);
+  let key = [];
+  let value = [];
+  for (var i = 0; i < array; i++) {
+    for (var j = 0; j < i[0]; j++) {
+      if (j === "") {
+        return key.push(j);
+      } else {
+        return value.push(j);
+      }
+    }
+  }
+  console.log(key, value);
 };
 const data = [
   ["a", 1],
   ["b", 2],
 ];
-fromPairs(data); // { 'a': 1, 'b': 2 }
+let value = fromPairs(data); // { 'a': 1, 'b': 2 }
+console.log(value);
