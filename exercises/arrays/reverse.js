418be52b5eca0e90f86b01344980397055ee1aa8
sulthananazeer <sulthanawaditwaiba@gmail.com>
2024-02-09 11:15:58 +0530

    complete reverse.js

--- exercises/arrays/reverse.js
+++ exercises/arrays/reverse.js
@@ -4,11 +4,12 @@
  * Task Complexity: 1 of 5
  * @param {Array} array - Array of any elements
  * @returns {Array}
- */
+ */ let reversed_Array = [];
 const reverse = (array) => {
-  for (let i = reverse.length - 1; i >= 0; i--) {
-    reverse.push([i]);
+  for (let i = array.length - 1; i >= 0; i--) {
+    reversed_Array.push(array[i]);
   }
+  console.log(reversed_Array);
 };
 const data = [1, 2, 3];
-console.log(reverse(data)); // [3, 2, 1]
+reverse(data); // [3, 2, 1]
