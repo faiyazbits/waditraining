9577a3ed68c8a41a869ce37bbe16bc1251701303
aznamolkn123 <aznawaditwaiba@gmail.com>
2024-02-09 11:45:45 +0530

    chop.js

--- exercises/string/chop.js
+++ exercises/string/chop.js
@@ -3,7 +3,13 @@
  * Expected Result: (javascript) =>  ["ja", "va", "sc", "ri", "pt"]
  * Task Complexity: 1 of 5
  */
-const chop = (str, chunk) => {};
+const chop = (str, chunk) => {
+  let chopedArray = [];
+  for (let i = 0; i < str.length; i = i + chunk) {
+    chopedArray.push(str.slice(i, i + chunk));
+  }
+  return chopedArray;
+};
 const str = "javascript";
 const chunk = 2;
 console.log(chop(str, chunk)); // ["ja", "va", "sc", "ri", "pt"]
