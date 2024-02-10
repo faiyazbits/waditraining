e0b728243b0041a56590cbce3167780e45d0a441
aznamolkn123 <aznawaditwaiba@gmail.com>
2024-02-10 11:47:47 +0530

    swapcase.js

--- exercises/string/swapcase.js
+++ exercises/string/swapcase.js
@@ -1,9 +1,22 @@
 /**
-  * Task description: Write a method that to converts upper case letters to lower case, and lower case letters to upper case.
-  * Expected Result: (AaBbc) => "aAbBC"
-  * Task Complexity: 1 of 5
-*/
+ * Task description: Write a method that to converts upper case letters to lower case, and lower case letters to upper case.
+ * Expected Result: (AaBbc) => "aAbBC"
+ * Task Complexity: 1 of 5
+ */
 const swapCase = (str) => {
-}
-const str = 'AaBbc'
-console.log(swapCase(str)) // "aAbBC"
+  let words = str.split("");
+  let output = [];
+  for (let letter of words) {
+    if (letter === letter.toUpperCase()) {
+      let smallLetter = letter.toLowerCase();
+      output.push(smallLetter);
+    } else {
+      let capitalLetter = letter.toUpperCase();
+      output.push(capitalLetter);
+    }
+  }
+
+  return output.join("");
+};
+const str = "AaBbc";
+console.log(swapCase(str)); // "aAbBC"
