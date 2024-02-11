0e581b031ddd3b051a0d09e20aff7f7524f2051a
naziya-usman <Naziyausmantwaiba@gmail.com>
2024-02-11 11:40:00 +0530

    completed keys.js

--- exercises/objects/keys.js
+++ exercises/objects/keys.js
@@ -5,7 +5,13 @@
  * @param {Object} data - The object to query.
  * @returns {Array} - Returns the array of property names.
  */
-const keys = (data) => {};
+const keys = (data) => {
+  let result = [];
+  for (let key in data) {
+    result.push(key);
+  }
+  return result;
+};
 const data = {
   a: 1,
   b: 2,
