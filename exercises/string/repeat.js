48d0156094c91453c85fb46eb1365a3f38431d8b
aznamolkn123 <aznawaditwaiba@gmail.com>
2024-02-12 11:27:07 +0530

    complete repeat.js

--- exercises/string/repeat.js
+++ exercises/string/repeat.js
@@ -1,10 +1,16 @@
 /**
-  * Task description: Write a method to concatenates a given string n times
-  * Expected Result: (JA) => "JA"
-  * Task Complexity: 1 of 5
-*/
-const repeat = (str,times) => {
-}
-const str = 'JA'
-const times = 2
-console.log(repeat(str, times)) // "JA JA"
+ * Task description: Write a method to concatenates a given string n times
+ * Expected Result: (JA) => "JA"
+ * Task Complexity: 1 of 5
+ */
+const repeat = (str, times) => {
+  let result = " ";
+  for (let i of str) {
+    result = str + str;
+    str = str + " ";
+  }
+  return result;
+};
+const str = "JA";
+const times = 2;
+console.log(repeat(str, times)); // "JA JA"
