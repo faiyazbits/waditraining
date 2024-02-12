5838daa38c75b2a5d9de049592a490898dca255a
aznamolkn123 <aznawaditwaiba@gmail.com>
2024-02-12 11:37:43 +0530

    complete truncate.js

--- exercises/string/truncate.js
+++ exercises/string/truncate.js
@@ -1,10 +1,12 @@
 /**
-  * Task description: Write a method to extract a specified number of characters from a string.
-  * Expected Result: (Robin Singh,4) => "Robi"
-  * Task Complexity: 1 of 5
-*/
-const truncate = (str,char) => {
-}
-const str = 'Robin Singh'
-const char = 4
-console.log(truncate(str,char)) // "Robi"
+ * Task description: Write a method to extract a specified number of characters from a string.
+ * Expected Result: (Robin Singh,4) => "Robi"
+ * Task Complexity: 1 of 5
+ */
+const truncate = (str, char) => {
+  let newString = str.slice(0, char);
+  return newString;
+};
+const str = "Robin Singh";
+const char = 4;
+console.log(truncate(str, char)); // "Robi"
